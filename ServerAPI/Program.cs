06912e33931b89e5440176b1ffa1bbc67c1df9db
NicklasEAA <eaa23nrfa@students.eaaa.dk>
2024-05-06 16:35:27 +0200

    cors

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using ServerAPI.Repositories;
+
 namespace ServerAPI;
 
 public class Program
@@ -10,23 +11,25 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("policy",
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin();
+                                  policy.AllowAnyMethod();
+                                  policy.AllowAnyHeader();
+                              });
+        });
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
 
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
-
 
         app.MapControllers();
 
---
+++ ServerAPI/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ServerAPI.Repositories
+{
+	public class UserRepository
+	{
+		public UserRepository()
+		{
+		}
+	}
+}
+
