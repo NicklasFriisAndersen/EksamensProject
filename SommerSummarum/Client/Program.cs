8a25336203fa677a0fa63e9723a0252e6f506ebd
vincentlorenzen <vincent.lorenzen@gmail.com>
2024-05-13 10:02:13 +0200

    loginvirker uden service

--- SommerSummarum/Client/Program.cs
+++ SommerSummarum/Client/Program.cs
@@ -21,14 +21,14 @@ public class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7016/") });
 
-        builder.Services.AddSingleton(sp => new HttpClient
-        {
-            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-        });
+        //builder.Services.AddSingleton(sp => new HttpClient
+        //{
+        //    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        //});
 
         builder.Services.AddBlazoredLocalStorage();
 
-        builder.Services.AddSingleton<IRegisterChildService, RegisterChildService>();
+       // builder.Services.AddSingleton<IRegisterChildService, RegisterChildService>();
 
 
         builder.Services
